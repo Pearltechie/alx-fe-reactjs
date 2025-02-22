5b3d65e956d7d943f305f5dd1100b38f6ecf7fa2
Pelumi Idowu <jehofawapelumi@gmail.com>
2025-02-22 20:50:38 +0100

    update UserContext.js

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import UserDetails from './components/UserDetails';
 
 
 
@@ -18,7 +19,6 @@ function App() {
   const userData = {
     name: "Alice",
     age: "25",
-    bio: "Loves hiking and photography",
   };
 
   return (
@@ -32,7 +32,8 @@ function App() {
             <Footer />
         </div>
         <div>
-      <UserProfile/>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <UserDetails /> {/* Render the UserDetails component */}
     </div>
       <div>
         <a href="https://vite.dev" target="_blank">

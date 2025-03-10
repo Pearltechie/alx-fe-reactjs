a1f0aa0592a9ade0e6a1b77a8942b2fe45486f67
Pelumi Idowu <jehofawapelumi@gmail.com>
2025-03-10 14:00:18 +0100

    updated app.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/post/:postId" element={<Post />} />
+          <Route path="/post/:blogId" element={<blogPost />} />
           <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
         </Routes>
       </Router>
